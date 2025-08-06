1ad5c85b6027f2301922cd57fc94c19c5f280b58
irynalopatina <lopatina.iryna@gmail.com>
2025-08-06 15:20:28 +0200

    solved class exercises

--- JavaScript/week1/pizza-exercise/pizza.js
+++ JavaScript/week1/pizza-exercise/pizza.js
@@ -1,6 +1,23 @@
 console.log("I love pizza");
 
-let pizzaName;
-let pizzaPrice;
+let pizzaName = "Margarita";
+let pizzaPrice = 135;
+
+// console.log("New pizza order: " + pizzaName + ".\nThe price of the pizza is: " + pizzaPrice + " DKK");
+
+let pizzaAmount = 7;
+let pizzaFamilySize = false;
+
+let pizzaType;
+if(pizzaFamilySize)
+{
+    pizzaPrice = pizzaPrice * 2;
+    pizzaType = " family ";
+}
+else pizzaType = " normal ";
+
+let totalPrice = pizzaAmount * pizzaPrice;
 
 
+
+console.log("New pizza order: \n" + pizzaAmount + pizzaType + pizzaName + ".\n" + "Total cost for the order: " + totalPrice + " DKK");
